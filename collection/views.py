d6162ae5bb3ebf1d7f6fac26de92367f09a40ff1
Sara Carothers <saracarothers@gmail.com>
2016-02-27 23:18:21 -0500

    Working subpages for all things

--- collection/views.py
+++ collection/views.py
@@ -4,7 +4,12 @@ from collection.models import Thing
 # Create your views here.
 def index(request):
 	things = Thing.objects.all()
-	# this is your view
 	return render(request, 'index.html', {
 		'things': things,
-		})
+	})
+
+def thing_detail(request, slug):
+	thing = Thing.objects.get(slug=slug)
+	return render(requ	est, 'things/thing_detail.html', {
+		'thing': thing,
+	})
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -25,5 +25,8 @@ urlpatterns = [
     url(r'^contact/$',
     	TemplateView.as_view(template_name='contact.html'),
     	name='contact'),
+    url(r'^things/(?P<slug>[-\w]+)/$',
+        'collection.views.thing_detail',
+        name='thing_detail'),
     url(r'^admin/', include(admin.site.urls)),
 ]
